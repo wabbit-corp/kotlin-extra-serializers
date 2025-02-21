24c4794324ed004d20c20a356aa48b057ea22b78
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2025-02-21 02:16:09 -0500

    Add ZonedDateTime and Instant serializers
    Introduces new KSerializer objects for ZonedDateTime and Instant using ISO-8601. Also updates LocalDateTimeSerializer to use an explicit ISO-8601 format and reorders dev-mode entries in .gitignore.
    
    Semver Impact: MINOR

--- src/main/kotlin/one/wabbit/serializers/LocalDateTimeSerializer.kt
+++ src/main/kotlin/one/wabbit/serializers/LocalDateTimeSerializer.kt
@@ -6,17 +6,47 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
-object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
-    override val descriptor: SerialDescriptor =
-        PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+object ZonedDateTimeSerializer : KSerializer<ZonedDateTime> {
+    override val descriptor = PrimitiveSerialDescriptor("ZonedDateTime", PrimitiveKind.STRING)
 
-    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+    override fun serialize(encoder: Encoder, value: ZonedDateTime) {
+        // Format the ZonedDateTime as an ISO-8601 string
+        encoder.encodeString(value.format(DateTimeFormatter.ISO_ZONED_DATE_TIME))
+    }
+
+    override fun deserialize(decoder: Decoder): ZonedDateTime {
+        // Parse the ISO-8601 string back to a ZonedDateTime
+        return ZonedDateTime.parse(decoder.decodeString(), DateTimeFormatter.ISO_ZONED_DATE_TIME)
+    }
+}
+
+object InstantSerializer : KSerializer<Instant> {
+    override val descriptor = PrimitiveSerialDescriptor("Instant", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: Instant) {
+        // Instant's default toString() produces an ISO-8601 representation
         encoder.encodeString(value.toString())
     }
 
+    override fun deserialize(decoder: Decoder): Instant {
+        return Instant.parse(decoder.decodeString())
+    }
+}
+
+object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+    override val descriptor = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        // Format the LocalDateTime as an ISO-8601 string (without timezone)
+        encoder.encodeString(value.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+    }
+
     override fun deserialize(decoder: Decoder): LocalDateTime {
-        return LocalDateTime.parse(decoder.decodeString())
+        return LocalDateTime.parse(decoder.decodeString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME)
     }
 }
